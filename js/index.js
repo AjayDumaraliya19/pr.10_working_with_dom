4a703093ab52c4ff5c1c454487cc8a101f3903d0
Ajay Dumaraliya <dumaraliyaajay1912@gmail.com>
2023-06-11 20:09:01 +0530

    completed

--- js/index.js
+++ js/index.js
@@ -1,35 +1,43 @@
-// Size change function
+// Size change function mouseover effect
 function sizeChange(element) {
-    let width = Math.floor(Math.random() * 300) + 100;
-    let height = Math.floor(Math.random() * 300) + 100;
-    element.style.width = width + "px";
-    element.style.height = height + "px";
+    let widths = Math.floor(Math.random() * 300) + 100;
+    let heights = Math.floor(Math.random() * 300) + 100;
+    element.style.width = widths + "px";
+    element.style.height = heights + "px";
 }
-// Size change reset function
+// Size change reset function mouseout effect
 function resetSize(element) {
     element.style.width = "20%";
     element.style.height = "50%";
 }
-// Image change function
+// Image change function mouseover effect
 function imgChange(element) {
-    let images = [
-        "img1.jpg",
-        "img2.jpg",
-        "img3.jpg",
-        "img4.jpg",
-        "img5.jpg",
-        "img6.jpg",
-        "img7.jpg",
-        "img8.jpg",
-        "img9.jpg"
-    ];
+    let images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg", "img8.jpg", "img9.jpg"];
     let randomIndex = Math.floor(Math.random() * images.length);
-    let randomImage ="./images/" + images[randomIndex];
+    let randomImage = "./images/" + images[randomIndex];
     element.style.backgroundImage = "url('" + randomImage + "')";
+
+    const changeContents = document.getElementById("image-content");
+    changeContents.innerHTML = "";
 }
 // Image change reset function
 function resetImg(element) {
     element.style.backgroundImage = "none";
+    let content = [
+        "The purpose of our lives is to be happy.",
+        "Life is what happens when you're busy making other plans.",
+        "Get busy living or get busy dying.",
+        "You only live once, but if you do it right, once is enough.",
+        "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
+        "If you want to live a happy life, tie it to a goal, not to people or things.",
+        "Never let the fear of striking out keep you from playing the game.",
+        "Money and success don’t change people; they merely amplify what is already there.",
+        "Not how long, but how well you have lived is the main thing."
+    ];
+    const changecontents = document.getElementById("image-content");
+    let randomcontentIndex = Math.floor(Math.random() * content.length);
+    let randomcontent = content[randomcontentIndex];
+    changecontents.innerHTML = randomcontent;
 }
 // Color change function
 function colorChange(element) {
